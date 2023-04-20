a78bbe4de0785dd44e11b86bce5046b818c7e381
Chirag Bhalotia <me@chirag.codes>
2023-04-21 05:21:02 +0530

    fixed swipe issue

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -10,20 +10,22 @@ import CarouselItem from "./CarouselItem";
 
 // example list
 const _list: CarouselItemProps[] = [
-  { id: 1, title: "video example", text:"description",video:"/assets/mov_bbb.mp4" },
+  {
+    id: 1,
+    title: "video example",
+    text: "description",
+    video: "/assets/mov_bbb.mp4",
+  },
   { id: 2, title: "2", image: "/assets/image.jpg" },
   { id: 3, title: "3", image: "/assets/image.jpg" },
   { id: 4, title: "4", image: "/assets/image.jpg" },
   { id: 5, title: "5", image: "/assets/image.jpg" },
   { id: 6, title: "6", image: "/assets/image.jpg" },
-  { id: 7, title: "7", image: "/assets/image.jpg" },
-  { id: 8, title: "8", image: "/assets/image.jpg" },
-  { id: 9, title: "9", image: "/assets/image.jpg" },
-  { id: 10, title: "10", image: "/assets/image.jpg" },
 ];
 
 function Carousel() {
   const ref = useRef<HTMLDivElement>(null);
+  let scroll = true;
   const [list, setList] = useState(_list); //to store the list of items
   const [itemNo, setItemNo] = useState(0); //to store the current item of the list
   const [canScroll, setCanScroll] = useState(true); //used to stop fast scrolling
@@ -34,14 +36,16 @@ function Carousel() {
   const setScrollTimeout = async () => {
     setTimeout(() => {
       setCanScroll(true);
+      scroll=true
     }, 550);
   };
 
-  const onMouseDown = (y:number) => {
+  const onMouseDown = (y: number) => {
+    // console.log("calling")
     setDragY(y);
   };
 
-  const onMouseUp= (y:number) => {
+  const onMouseUp = (y: number) => {
     if (!dragY) return;
     if (y < dragY) {
       nextItem();
@@ -49,40 +53,49 @@ function Carousel() {
     if (y > dragY) {
       previousItem();
     }
-    setDragY(null)
+    setDragY(null);
   };
 
   //handles scroll
   const onWheel: WheelEventHandler = (evt) => {
     if (!canScroll) return;
+    console.log(canScroll);
 
     if (evt.deltaY > 0) {
       nextItem();
     } else if (evt.deltaY < 0) {
       previousItem();
     }
-    setCanScroll(false);
-    setScrollTimeout();
   };
 
   const nextItem = () => {
     // adding new items and deleting old items on reaching the end
+    if (!canScroll || !scroll) return;
     if (itemNo > list.length - 4) {
       setList((old) => {
         return removeOldItem(addNewItem(old));
       });
+      setCanScroll(false);
+      scroll = false;
+      setScrollTimeout();
       return;
     }
 
     setItemNo((old) => old + 1);
+    scroll = false;
+    setCanScroll(false);
+    setScrollTimeout();
   };
 
   //moves the carousel to the previous item
   const previousItem = () => {
+    if (!canScroll || !scroll) return;
     if (itemNo === 0) return;
     setItemNo((old) => old - 1);
+    scroll = false;
+    setCanScroll(false);
+    setScrollTimeout();
   };
-
 
   //removes old items to save memory
   const removeOldItem = (list: any[]) => {
@@ -91,9 +104,14 @@ function Carousel() {
 
   //adds new items on reaching near the end
   const addNewItem = (list: any[]) => {
+    console.log("adding", canScroll);
     return [
       ...list,
-      { id: Date.now(), title: Date.now() + "", image: "/assets/image.jpg" },
+      {
+        id: Date.now() + "" + Math.random(),
+        title: Date.now() + "",
+        image: "/assets/image.jpg",
+      },
     ];
   };
 
@@ -112,11 +130,11 @@ function Carousel() {
       }}
       onTouchMove={(evt) => {
         evt.preventDefault();
-        onMouseUp(evt.touches[0].clientY)
+        onMouseUp(evt.touches[0].clientY);
       }}
       onWheel={onWheel}
-      onMouseDown={e => onMouseDown(e.clientY)}
-      onMouseUp={e => onMouseUp(e.clientY)}
+      onMouseDown={(e) => onMouseDown(e.clientY)}
+      onMouseUp={(e) => onMouseUp(e.clientY)}
       className={`w-full h-full overflow-hidden relative  `}
     >
       {ref.current &&
@@ -126,7 +144,7 @@ function Carousel() {
               key={item.id}
               {...item}
               top={(index - itemNo) * (height as number)}
-              currentItem={(index === itemNo)}
+              currentItem={index === itemNo}
             />
           );
         })}
--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -1,57 +1,77 @@
 import { useEffect, useRef, useState } from "react";
+import ReactInstaStories from "react-insta-stories";
 import { CarouselItemProps } from "src/types";
 
 function CarouselItem(props: CarouselItemProps) {
-    const ref = useRef<HTMLVideoElement>(null)
-    const [isPlaying,setIsPlaying] = useState(false) //to check if video is playing or not
-
-    //to toggle video on Click
-    const togglePlay = () => {
-
-      if(isPlaying){
-        setIsPlaying(false)
-        ref.current?.pause()
-      } else {
-        setIsPlaying(true)
-        ref.current?.play()
-      }
-    }
-    useEffect(() => {
-      //plays or pauses the video if the video is displayed on the screen
-      if(props.currentItem && ref.current){
-        ref.current.play()
-        setIsPlaying(true)
-      } else {
-        ref.current?.pause()
-        setIsPlaying(false)
-      }
-    },[props.currentItem])
   return (
     <div
       style={{ top: props.top ?? 0 }}
       className="h-full w-full py-5 absolute transition-all duration-500"
     >
-      <div
-        style={props.image ? { backgroundImage: `url(${props.image})` } : {}}
-        className="content relative bg-yellow-100 rounded-3xl h-full w-full flex justify-center bg-cover bg-center overflow-hidden"
-      >
-        {props.video && (
-          <video
-            ref={ref}
-            className="video-stream html5-main-video h-full w-full object-cover"
-            data-no-fullscreen="true"
-            src="/assets/mov_bbb.mp4"
-            onClick={togglePlay}
-            loop={true}
-          ></video>
-        )}
-        <div className="title absolute bottom-10 left-5">
-          <p className="text-3xl text-white ">{props.title}</p>
-          <p className="description text-gray-50 text-xl">{props.text}</p>
-        </div>
-      </div>
+      {/* <ReactInstaStories
+        stories={[
+          {
+            content: (
+              
+            ),
+          },
+        ]}
+        defaultInterval={1500}
+        // width={432}
+        // height={768}
+        width={"100%"}
+        // ={{margin:"auto"}}
+      /> */}
+      <Story {...props} />
     </div>
   );
 }
 
 export default CarouselItem;
+
+function Story(props: CarouselItemProps) {
+  const ref = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false); //to check if video is playing or not
+
+  //to toggle video on Click
+  const togglePlay = () => {
+    if (isPlaying) {
+      setIsPlaying(false);
+      ref.current?.pause();
+    } else {
+      setIsPlaying(true);
+      ref.current?.play();
+    }
+  };
+  useEffect(() => {
+    //plays or pauses the video if the video is displayed on the screen
+    if (props.currentItem && ref.current) {
+      ref.current.play();
+      setIsPlaying(true);
+    } else {
+      ref.current?.pause();
+      setIsPlaying(false);
+    }
+  }, [props.currentItem]);
+  return (
+    <div
+      style={props.image ? { backgroundImage: `url(${props.image})` } : {}}
+      className="content relative bg-yellow-100 rounded-3xl h-full w-full flex justify-center bg-cover bg-center overflow-hidden"
+    >
+      {props.video && (
+        <video
+          ref={ref}
+          className="video-stream html5-main-video h-full w-full object-cover"
+          data-no-fullscreen="true"
+          src="/assets/mov_bbb.mp4"
+          onClick={togglePlay}
+          loop={true}
+        ></video>
+      )}
+      <div className="title absolute bottom-10 left-5">
+        <p className="text-3xl text-white ">{props.title}</p>
+        <p className="description text-gray-50 text-xl">{props.text}</p>
+      </div>
+    </div>
+  );
+}
